ba5f09ed4afb974c38027d2ec0b89c262000ce62
imshaikot <mail2shariar@gmail.com>
2018-01-19 17:53:35 +0600

    added string converter

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-export default class {
-  constructor(resource) {}
+class WebVTTConverter {
+  constructor(resource) {
+    this.resource = resource;
+  }
+
+  blobToBuffer() {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.addEventListener('loadend', event => {
+        const buf = event.target.result;
+        resolve(new Uint8Array(buf));
+      });
+      reader.addEventListener('error', () => reject('Error while reading the Blob object'));
+      reader.readAsArrayBuffer(this.resource)
+    });
+  }
+
+  toVTT(utf8str) {
+    return utf8str
+    .replace(/\{\\([ibu])\}/g, '</$1>')
+    .replace(/\{\\([ibu])1\}/g, '<$1>')
+    .replace(/\{([ibu])\}/g, '<$1>')
+    .replace(/\{\/([ibu])\}/g, '</$1>')
+    .replace(/(\d\d:\d\d:\d\d),(\d\d\d)/g, '$1.$2') + '\r\n\r\n';
+  }
+
+  getURL() {
+    return new Promise((resolve, reject) => {
+      if (!(this.resource instanceof Blob)) return reject('Expecting resource to be a Blob but something else found.');
+      if (!(FileReader)) return reject('No FileReader constructor found');
+      if (!TextDecoder) return reject('No TextDecoder constructor found');
+      this.blobToBuffer()
+      .then(buffer => {
+        const utf8str = new TextDecoder('utf-8').decode(buffer);
+        const vttString = 'WEBVTT FILE\r\n\r\n';
+      });
+    });
+  }
 }
+
+window.WebVTTConverter = WebVTTConverter;
+
+export default WebVTTConverter;
